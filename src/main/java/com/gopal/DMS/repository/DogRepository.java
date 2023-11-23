00b82cbd960e09b708cfc78d516c1435a3acf576
CH V V GOPAL RAJU KALIDINDI <S559007@NWMISSOURI.EDU>
2023-11-23 12:00:35 -0600

    committed files

---
+++ src/main/java/com/gopal/DMS/repository/DogRepository.java
@@ -0,0 +1,14 @@
+//@Author- Gopal Raju Kalidindi
+
+
+package com.gopal.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gopal.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
+	List<Dog> findByName(String name);
+}
---
+++ src/main/java/com/gopal/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.gopal.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.gopal.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
